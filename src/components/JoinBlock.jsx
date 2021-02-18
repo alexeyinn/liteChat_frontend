0fa3b541edecf9b54a83958778f1b03c1c8a4694
alexeyinn <69390951+alexeyinn@users.noreply.github.com>
2021-02-18 14:24:55 +0300

    feat: Добавили переменные для вводимых данных

--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -3,10 +3,23 @@ import React from "react";
 import socket from "../socket";
 
 export default function JoinBlock() {
+  const [roomId, setRoomId] = React.useState("");
+  const [userName, setUserName] = React.useState("");
+
   return (
     <div className="join-block">
-      <input type="text" placeholder="Room ID" />
-      <input type="text" placeholder="Ваше имя" />
+      <input
+        type="text"
+        placeholder="Room ID"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Ваше имя"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
       <button className="btn btn-success" onClick={socket}>
         ВОЙТИ
       </button>
